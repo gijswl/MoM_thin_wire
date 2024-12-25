ef83f874fbba965859986513fe387ea3bb1338bc
Gijs Lagerweij <g.w.lagerweij@student.tudelft.nl>
2024-12-25 13:11:06 +0100

    Improve flow of matrix assembly; add more comments to procedure

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -7,8 +7,10 @@ using LinearAlgebra
 Dyadic Green's function with wavenumber `k`.
 """
 function G(k, rm, rn)
-    r = norm(rm .- rn)
-    return exp(-1im * k * r) / (4π * r)
+    @timeit "Green's function" begin
+        r = norm(rm - rn)
+        return exp(-1im * k * r) / (4π * r)
+    end
 end
 
 """
@@ -21,7 +23,7 @@ function S1(x, s, a, k)
 end
 
 """
-    Ss(x, s, a, k)
+    S2(x, s, a, k)
 
 Analytic integral ∫ G(x, r) dr on segment `(x, s)` with wire radius `a` and wavenumber `k`.
 """
@@ -63,54 +65,86 @@ function assemble_global(msh::Mesh, src::Vector{<:Source}, f::Real, μ::Real, ε
             ℓm = norm(rm)
             Eel[s.edge] += s.V / ℓm
         end
-    end
-
-    # Define quadrature
-    quad = get_quadrature(quad_order)
+    end    
 
     # Assemble system equation Z * I = V
     ndofs = size(msh.nodes, 1)
     Z = zeros(Complex{Float64}, ndofs, ndofs)
     V = zeros(Complex{Float64}, ndofs)
 
-    @timeit "outer loop" for (m, em) ∈ enumerate(msh.edges)
-        nm = msh.nodes[em, :]
-        rm = nm[2, :] .- nm[1, :]
-        ℓm = norm(rm)
-        bm = msh.basis_vecs[m, :]
+    quad = get_quadrature(quad_order)
 
-        V[em] += [1; 1] * 0.5 * Eel[m] * ℓm
+    # Pre-compute
+    ne = map(e -> nodes[e, :], msh.edges) # Nodal positions
+    r = map(n -> n[2, :] - n[1, :], ne)   # Element vectors
+    ℓ = map(r -> norm(r), r)              # Element length
 
-        for (n, en) ∈ enumerate(msh.edges)
-            nn = msh.nodes[en, :]
-            rn = nn[2, :] .- nn[1, :]
-            ℓn = norm(rn)
-            bn = msh.basis_vecs[n, :]
+    # Local element contributions
+    Amn = [0 0; 0 0]
+    Φmn = [0 0; 0 0]
 
-            Amn = [0 0; 0 0]
-            Φmn = [0 0; 0 0]
+    # Loop over the source elements
+    @timeit "outer loop" @inbounds @views for (m, em) ∈ enumerate(msh.edges)
+        # Parameters of element m
+        nm = ne[m]                # Nodal positions [m1; m2]
+        ℓm = ℓ[m]                 # Length of element m
+        bm = msh.basis_vecs[m, :] # Basis vector on element m
 
-            @timeit "element" for (p, qp) ∈ enumerate(quad)
-                ξp, wp = qp
-                rp = 0.5 * (ξp + 1) .* rm .+ nm[1, :]
-                ζp = 0.5 * (ξp + 1) * ℓm
+        # Assemble the source vector V
+        # Apply quadrature over the elements to calculate Vm = ∫ f(r) ⋅ E(r) dr
+        # We assume that the electric field in vector Eel is oriented along the wire axis.
+        @timeit "source" for qp ∈ quad
+            ξp, wp = qp
+            fm1 = 0.5 * (1 - ξp)
+            fm2 = 0.5 * (1 + ξp)
 
-                fm1 = 0.5 * (1 - ξp)
-                fm2 = 0.5 * (1 + ξp)
+            V[em] += 0.5 * ℓm * wp * [fm1; fm2] .* Eel[m]
+        end
 
-                if (m == n)
-                    Amn += 0.5 * ℓm * wp * [fm1 fm1; fm2 fm2] * S1(ζp, ℓm, a, k)
-                    Φmn += 0.5 * ℓm * wp * [1 -1; -1 1] * S2(ζp, ℓm, a, k)
-                else
-                    for (q, qq) ∈ enumerate(quad)
-                        ξq, wq = qq
-                        rq = 0.5 * (ξq + 1) .* rn .+ nn[1, :]
+        # Loop over the receiving elements
+        @timeit "inner loop" @inbounds @views for (n, en) ∈ enumerate(msh.edges)
+            # Parameters of element n
+            nn = ne[n]                # Nodal positions [n1; n2]
+            ℓn = ℓ[n]                 # Length of element n
+            bn = msh.basis_vecs[n, :] # Basis vector on element n
 
-                        fn1 = 0.5 * (1 - ξq)
-                        fn2 = 0.5 * (1 + ξq)
+            bdot = bm ⋅ bn
 
-                        Amn += 0.25 * ℓm * ℓn * wp * wq * (bm ⋅ bn) * [(fm1*fn1) (fm1*fn2); (fm2*fn1) (fm2*fn2)] * G(k, rp, rq)
-                        Φmn += 0.25 * wp * wq * [1 -1; -1 1] * G(k, rp, rq)
+            @timeit "element" begin
+                # Reset element contribution matrix
+                fill!(Amn, 0)
+                fill!(Φmn, 0)
+
+                @inbounds for qp ∈ quad
+                    ξp, wp = qp
+
+                    fm1 = 0.5 * (1 - ξp)
+                    fm2 = 0.5 * (1 + ξp)
+                    rp = fm1 * nm[1, :] + fm2 * nm[2, :]
+
+                    # If the source and receive elements overlap (m == n), we evaluate 
+                    # the inner integral analytically and the outer numerically.
+                    if (m == n)
+                        @timeit "self terms" begin
+                            ζp = 0.5 * (ξp + 1) * ℓm
+
+                            Amn += 0.5 * ℓm * wp * [fm1 fm1; fm2 fm2] * S1(ζp, ℓm, a, k)
+                            Φmn += 0.5 * ℓm * wp * [1 -1; -1 1] * S2(ζp, ℓm, a, k)
+                        end
+
+                    else # Otherwise, evaluate both integrals numerically.
+                        @timeit "non-self terms" begin
+                            @inbounds for qq ∈ quad
+                                ξq, wq = qq
+
+                                fn1 = 0.5 * (1 - ξq)
+                                fn2 = 0.5 * (1 + ξq)
+                                rq = fn1 * nn[1, :] + fn2 * nn[2, :]
+
+                                Amn += 0.25 * ℓm * ℓn * wp * wq * bdot * [(fm1*fn1) (fm1*fn2); (fm2*fn1) (fm2*fn2)] * G(k, rp, rq)
+                                Φmn += 0.25 * wp * wq * [1 -1; -1 1] * G(k, rp, rq)
+                            end
+                        end
                     end
                 end
             end
