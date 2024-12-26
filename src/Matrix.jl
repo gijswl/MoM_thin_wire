18467da4b2a50264de37da2882d6a2146aa55ecf
Gijs Lagerweij <g.w.lagerweij@student.tudelft.nl>
2024-12-26 20:12:07 +0100

    Improved matrix assembly performance by type stability and @views

--- src/Matrix.jl
+++ src/Matrix.jl
@@ -7,10 +7,8 @@ using LinearAlgebra
 Dyadic Green's function with wavenumber `k`.
 """
 function G(k, rm, rn)
-    @timeit "Green's function" begin
-        r = norm(rm - rn)
-        return exp(-1im * k * r) / (4π * r)
-    end
+    r = norm(rm .- rn)
+    return exp(-1im * k * r) / (4π * r)
 end
 
 """
@@ -42,6 +40,48 @@ function get_quadrature(order)
     return [[x[i], w[i]] for i = eachindex(x)]
 end
 
+function assemble!(Amn::Matrix{<:Complex}, Φmn::Matrix{<:Complex}, quad::Vector{Vector{Float64}}, m, nm, ℓm, bm, n, nn, ℓn, bn, a, k)
+    # Reset element contribution matrix
+    fill!(Amn, 0)
+    fill!(Φmn, 0)
+
+    nm1 = @view nm[1, :]
+    nm2 = @view nm[2, :]
+    nn1 = @view nn[1, :]
+    nn2 = @view nn[2, :]
+
+    @inbounds for qp ∈ quad
+        ξp = qp[1]; wp = qp[2]
+
+        fm1 = 0.5 * (1 - ξp)
+        fm2 = 0.5 * (1 + ξp)
+        rp = fm1 .* nm1 .+ fm2 .* nm2
+
+        # If the source and receive elements overlap (m == n), we evaluate 
+        # the inner integral analytically and the outer numerically.
+        if (m == n)
+            ζp = 0.5 * (ξp + 1) * ℓm
+
+            Amn .+= 0.5 * ℓm * wp * S1(ζp, ℓm, a, k) .* [fm1 fm1; fm2 fm2]
+            Φmn .+= 0.5 * ℓm * wp * S2(ζp, ℓm, a, k) .* [1 -1; -1 1]
+
+        else # Otherwise, evaluate both integrals numerically.
+            @inbounds for qq ∈ quad
+                ξq = qq[1]; wq = qq[2]
+
+                fn1 = 0.5 * (1 - ξq)
+                fn2 = 0.5 * (1 + ξq)
+                rq = fn1 .* nn1 .+ fn2 .* nn2
+
+                Amn .+= 0.25 * ℓm * ℓn * wp * wq * (bm ⋅ bn) * G(k, rp, rq) .* [(fm1*fn1) (fm1*fn2); (fm2*fn1) (fm2*fn2)]
+                Φmn .+= 0.25 * wp * wq * G(k, rp, rq) .* [1 -1; -1 1]
+            end
+        end
+    end
+
+    return Amn, Φmn
+end
+
 """
     assemble_global(msh::Mesh, src::Vector{<:Source}, f::Real, μ::Real, ε::Real; quad_order=4)
 
@@ -61,11 +101,11 @@ function assemble_global(msh::Mesh, src::Vector{<:Source}, f::Real, μ::Real, ε
     for s ∈ src
         if (typeof(s) == VoltageSource)
             nm = msh.nodes[msh.edges[s.edge], :]
-            rm = nm[2, :] .- nm[1, :]
+            rm = nm[2, :] - nm[1, :]
             ℓm = norm(rm)
             Eel[s.edge] += s.V / ℓm
         end
-    end    
+    end
 
     # Assemble system equation Z * I = V
     ndofs = size(msh.nodes, 1)
@@ -75,13 +115,13 @@ function assemble_global(msh::Mesh, src::Vector{<:Source}, f::Real, μ::Real, ε
     quad = get_quadrature(quad_order)
 
     # Pre-compute
-    ne = map(e -> nodes[e, :], msh.edges) # Nodal positions
+    ne = map(e -> msh.nodes[e, :], msh.edges) # Nodal positions
     r = map(n -> n[2, :] - n[1, :], ne)   # Element vectors
     ℓ = map(r -> norm(r), r)              # Element length
 
     # Local element contributions
-    Amn = [0 0; 0 0]
-    Φmn = [0 0; 0 0]
+    Amn = zeros(Complex{Float64}, 2, 2)
+    Φmn = zeros(Complex{Float64}, 2, 2)
 
     # Loop over the source elements
     @timeit "outer loop" @inbounds @views for (m, em) ∈ enumerate(msh.edges)
@@ -98,7 +138,7 @@ function assemble_global(msh::Mesh, src::Vector{<:Source}, f::Real, μ::Real, ε
             fm1 = 0.5 * (1 - ξp)
             fm2 = 0.5 * (1 + ξp)
 
-            V[em] += 0.5 * ℓm * wp * [fm1; fm2] .* Eel[m]
+            V[em] += 0.5 * ℓm * wp * Eel[m] .* [fm1; fm2]
         end
 
         # Loop over the receiving elements
@@ -108,47 +148,8 @@ function assemble_global(msh::Mesh, src::Vector{<:Source}, f::Real, μ::Real, ε
             ℓn = ℓ[n]                 # Length of element n
             bn = msh.basis_vecs[n, :] # Basis vector on element n
 
-            bdot = bm ⋅ bn
-
-            @timeit "element" begin
-                # Reset element contribution matrix
-                fill!(Amn, 0)
-                fill!(Φmn, 0)
-
-                @inbounds for qp ∈ quad
-                    ξp, wp = qp
-
-                    fm1 = 0.5 * (1 - ξp)
-                    fm2 = 0.5 * (1 + ξp)
-                    rp = fm1 * nm[1, :] + fm2 * nm[2, :]
-
-                    # If the source and receive elements overlap (m == n), we evaluate 
-                    # the inner integral analytically and the outer numerically.
-                    if (m == n)
-                        @timeit "self terms" begin
-                            ζp = 0.5 * (ξp + 1) * ℓm
-
-                            Amn += 0.5 * ℓm * wp * [fm1 fm1; fm2 fm2] * S1(ζp, ℓm, a, k)
-                            Φmn += 0.5 * ℓm * wp * [1 -1; -1 1] * S2(ζp, ℓm, a, k)
-                        end
-
-                    else # Otherwise, evaluate both integrals numerically.
-                        @timeit "non-self terms" begin
-                            @inbounds for qq ∈ quad
-                                ξq, wq = qq
-
-                                fn1 = 0.5 * (1 - ξq)
-                                fn2 = 0.5 * (1 + ξq)
-                                rq = fn1 * nn[1, :] + fn2 * nn[2, :]
-
-                                Amn += 0.25 * ℓm * ℓn * wp * wq * bdot * [(fm1*fn1) (fm1*fn2); (fm2*fn1) (fm2*fn2)] * G(k, rp, rq)
-                                Φmn += 0.25 * wp * wq * [1 -1; -1 1] * G(k, rp, rq)
-                            end
-                        end
-                    end
-                end
-            end
-            Z[em, en] += 1im * ω * μ * Amn - 1im / (ω * ε) * Φmn
+            @timeit "element" assemble!(Amn, Φmn, quad, m, nm, ℓm, bm, n, nn, ℓn, bn, a, k)
+            Z[em, en] += 1im * ω * μ .* Amn - 1im / (ω * ε) .* Φmn
         end
     end
 
